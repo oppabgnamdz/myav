5c4f3c53b2f256d1d99f0ac2be49071b1f15da95
oppabgnamdz <oppabgnamdz@gmail.com>
2020-07-17 23:30:02 +0700

    fix

--- src/Card.js
+++ src/Card.js
@@ -19,14 +19,14 @@ export default class Card extends Component {
 
         let index = this.props.keys
         return (
-            <div 
-            onClick={() => this.props.click(index)}
-            href={this.props.href}
-            className="bg-light-green dib  br3 pa3 ma2 grow bw2 shadow-5 mt5" style={{ width: '350px' }}>
+            <div
+                onClick={() => this.props.click(index)}
+                href={this.props.href}
+                className="bg-light-green dib  br3 pa3 ma2 grow bw2 shadow-5 mt5" style={{ width: '350px' }}>
                 <h1 style={{ textOverflow: 'ellipsis', overflow: 'hidden' }}>{this.props.name}</h1>
                 <h1 style={{ color: 'orange' }}>{this.props.title}</h1>
                 <div
-                   >
+                >
                     <video
                         title={this.props.title}
                         style={{ width: '300px', height: '200px', backgroundColor: 'black', }}
